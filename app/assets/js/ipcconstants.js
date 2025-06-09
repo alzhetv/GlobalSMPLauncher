99a89133748edd9b67b276d90f608b1f5e90cf10
POKAMENI <88514584+pokameni@users.noreply.github.com>
2025-06-09 20:05:47 +0200

    1.0.0, 1.0.1, 1.0.2 refractor

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = '16e64bf4-e2ca-4202-8710-d29cdb17ac99'
 // SEE NOTE ABOVE.
 
 
