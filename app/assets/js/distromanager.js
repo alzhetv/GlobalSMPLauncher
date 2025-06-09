bd1d4e38a2bbe256a431561c4fcbca6000501573
POKAMENI <88514584+pokameni@users.noreply.github.com>
2025-06-09 20:57:53 +0200

    fix(distro) wordpress redirect rules made me create a new webserver for theses

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://www.alzhe.fr/globalsmp/launcher/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://glowstone.alzhe.fr/globalsmp/launcher/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
